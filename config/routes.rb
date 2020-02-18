d2027b664c0d723edf47c4258e15bedbe99b5150
thethinh <nguyenthethinh16011998@Gmail.com>
2020-02-18 16:54:03 +0700

    signup complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # root routes
+  root 'static_pages#home'
+
+  # GET routes
+  get  '/signup',  to: 'users#new'
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'static_pages#home'
+
+  #POST routes
+  post '/signup',  to: 'users#create'
+
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
