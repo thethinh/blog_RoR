458e8ce3f3eb5f856bad941b8bcf587cb276a079
thethinh <nguyenthethinh16011998@Gmail.com>
2020-02-25 09:30:44 +0700

    edit indent, method convention and use presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,131 +1,126 @@
 class User < ApplicationRecord
-    has_many :microposts,  dependent: :destroy
-    has_many :active_relationships, class_name:  "Relationship",
-                                    foreign_key: "follower_id",
-                                    dependent:   :destroy
-    has_many :passive_relationships, class_name:  "Relationship",
-                                    foreign_key: "followed_id",
-                                    dependent:   :destroy
-    has_many :following, through: :active_relationships, source: :followed
-    has_many :followers, through: :passive_relationships, source: :follower
-    attr_accessor :remember_token, :activation_token, :reset_token
-    before_save   :downcase_email
-    before_create :create_activation_digest
-    validates :name,  presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 }, 
-                      format: { with: VALID_EMAIL_REGEX },
-                      uniqueness: { case_sensitive: false }
-    has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-    scope :calculation_oneweek, -> { where(created_at: (1.week.ago.beginning_of_day)..Time.zone.now.end_of_day) }
+  has_many :microposts,  dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, 
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  scope :calculation_oneweek, -> { where(created_at: (1.week.ago.beginning_of_day)..Time.zone.now.end_of_day) }
 
 
-    def self.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                      BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
-    end
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
-    # Returns a random token.
-    def self.new_token
-        SecureRandom.urlsafe_base64
-    end
+  # Returns a random token.
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
 
-    def remember
-        self.remember_token = User.new_token
-        update_attribute(:remember_digest, User.digest(remember_token))   
-    end
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))   
+  end
 
-    # Forgets a user.
-    def forget
-        update_attribute(:remember_digest, nil)
-    end
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 
-    # Returns true if the given token matches the digest.
-    def authenticated?(attribute, token)
-        digest = send("#{attribute}_digest")
-        return false if digest.nil?
-        BCrypt::Password.new(digest).is_password?(token)
-    end
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
-    # Activates an account.
-    def activate
-        update_attribute(:activated,    true)
-        update_attribute(:activated_at, Time.zone.now)
-    end
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
 
-    # Sends activation email.
-    def send_activation_email
-        UserMailer.account_activation(self).deliver_now
-    end
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
-    # Sends auto email to mail user
-    def self.send_auto_email
-        User.all.each{|user| ScheduleMailer.auto_sendmail(user).deliver}
-    end
-       
+  # Sends auto email to mail user
+  def self.send_auto_email
+    User.all.each{|user| ScheduleMailer.auto_sendmail(user).deliver}
+  end
+      
 
-    # Sets the password reset attributes.
-    def create_reset_digest
-        self.reset_token = User.new_token
-        update_attribute(:reset_digest,  User.digest(reset_token))
-        update_attribute(:reset_sent_at, Time.zone.now)
-    end
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
 
-    # Sends password reset email.
-    def send_password_reset_email
-        UserMailer.password_reset(self).deliver_now
-    end
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
 
-    # Returns true if a password reset has expired.
-    def password_reset_expired?
-        reset_sent_at < 2.hours.ago
-    end
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
-    def feed
-        Micropost.where("user_id = ?", id)
-    end
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
-    # Follows a user.
-    def follow(other_user)
-        following << other_user
-    end
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
 
-    # Unfollows a user.
-    def unfollow(other_user)
-        following.delete(other_user)
-    end
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
 
-    # Returns true if the current user is following the other user.
-    def following?(other_user)
-        following.include?(other_user)
-    end
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
-    def self.from_omniauth(auth)
-        # create new user if doesn't exist user email
-        user = User.find_by(email: auth.info.email)
-        if user 
-            user
-        else
-            user = User.new(
+  def self.from_omniauth(auth)
+    # create new user if doesn't exist user email
+    user = User.find_by(email: auth.info.email)
+    user = user.presence || User.new(
                 name: auth.info.name,
                 email: auth.info.email,
                 password: auth.uid,
                 password_confirmation: auth.uid,
-                activated: true # activated account because it's authentication from google
-            )
-        end
-    end
+                activated: true )# activated account because it's authentication from google
+  end
 
-    private
-        # Converts email to all lower-case.
-        def downcase_email
-            self.email = email.downcase
-        end
-        # Creates and assigns the activation token and digest.
-        def create_activation_digest
-            self.activation_token  = User.new_token
-            self.activation_digest = User.digest(activation_token)
-        end
+  private
+    # Converts email to all lower-case.
+    def downcase_email
+        self.email = email.downcase
+    end
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+        self.activation_token  = User.new_token
+        self.activation_digest = User.digest(activation_token)
+    end
 end
