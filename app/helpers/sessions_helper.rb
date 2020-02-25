2d0d5dc6393cb0c85d020cfe2923a3c637c28b68
thethinh <nguyenthethinh16011998@Gmail.com>
2020-02-25 15:31:55 +0700

    edit ++++

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -48,13 +48,13 @@ module SessionsHelper
   end
   
   def redirect_back_or(default)
-		redirect_to(session[:forwarding_url] || default)
-		session.delete(:forwarding_url)
-	end
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
 	
-	# Stores the URL trying to be accessed.
-	def store_location
-		session[:forwarding_url] = request.original_url if request.get?
-	end
-    
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end  
+  
 end
