efb427a6bd591af5e519565c331532ff8ab74ddf
thethinh <nguyenthethinh16011998@Gmail.com>
2020-02-25 15:16:48 +0700

    edit once

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,10 +1,10 @@
 class MicropostsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
 	before_action :correct_user,   only: [:destroy]
-	
+
 	def create
-		@micropost = current_user.microposts.build(micropost_params)
-		if @micropost.save
+			@micropost = current_user.microposts.build(micropost_params)
+			if @micropost.save
 			flash[:success] = "Micropost created!"
 			redirect_to root_url
 		else
