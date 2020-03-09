c2e527bc24a40a62c645a03a5e71904fb2c4fae3
thethinh <nguyenthethinh16011998@Gmail.com>
2020-03-09 16:16:08 +0700

    change from review

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,23 +4,6 @@
 //= require turbolinks
 //= require_tree .
 
-// function showcmt(id){
-//   var cmt = document.getElementsByClassName('area-cmt');
-
-//   $.ajax({
-//     type: 'GET',
-//     url: `/microposts/${id}/comments`,
-//     data: {count_cmt: cmt.length},
-//     dataType: 'json',
-//     success: function(result){
-//       console.log(result);
-//       $("#"+id).append('<div>hello</div>');
-//     }
-//   });
-
-// }
-
-
 
 
  
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
   def index
-    currentSumCmt = params[:currentSumCmt].to_i
+    current_Sum_Cmt = params[:currentSumCmt].to_i
     @post = Micropost.find(params[:micropost_id])
 
     if(params[:currentSumCmt])
-      @comment = @post.comment.first(currentSumCmt + 4)
+      # Nếu có số comment truyền lên thì lấy ra số  cmt = số cmt hiện tại + 4
+      @comment = @post.comment.first(current_Sum_Cmt + 4)
     else
+      #Nếu chưa có params truyền lên(lúc ban đầu) thì lấy ra 7 cmt (số comment ban đầu( mặc định 3) + 4)
       @comment = @post.comment.first(7)
     end
     respond_to do |format|
