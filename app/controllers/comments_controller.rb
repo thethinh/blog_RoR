3962c91619c78216a9fb298352c26edb50b658c6
thethinh <nguyenthethinh16011998@Gmail.com>
2020-03-09 15:03:01 +0700

    complete and fix errors display comment inside post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
   def index
+    currentSumCmt = params[:currentSumCmt].to_i
     @post = Micropost.find(params[:micropost_id])
-    @comment = @post.comment
+
+    if(params[:currentSumCmt])
+      @comment = @post.comment.first(currentSumCmt + 4)
+    else
+      @comment = @post.comment.first(7)
+    end
     respond_to do |format|
       format.html {redirect_to @comment}
       format.js
