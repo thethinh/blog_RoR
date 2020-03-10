54e7bb807c8eca54d6321d4378a2a11891db8526
thethinh <nguyenthethinh16011998@Gmail.com>
2020-03-10 17:22:27 +0700

    complete create,edit,delete comment

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,13 @@
 module CommentsHelper
+  # Kiểm tra xem comment có phải của curent_user không
+  def comment_of_user?(comment)
+    comment.user.id == current_user.id
+  end
+  
+  # Kiểm tra xem comment có nằm trong post của curent user không
+  def comment_in_post_user?(comment)
+    comment.micropost == current_user.microposts.find_by(id: comment.micropost_id)
+  end
+  
+  
 end
