87d8e7787c9e6db54a3454478dde9e1d50841408
thethinh <nguyenthethinh16011998@Gmail.com>
2020-03-29 16:15:14 +0700

    edit delete rule seubcomment

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -8,6 +8,10 @@ module CommentsHelper
   def comment_in_post_user?(comment)
     current_user.microposts.find_by(id: comment.micropost_id).present?
   end
-  
+
+  # Kiểm tra xem comment reply có thuộc comment cha của curent user khong
+  def subcmt_of_cmt_current_user?(comment)
+    current_user.comment.find_by(id: comment.comment_id).present?
+  end
   
 end
